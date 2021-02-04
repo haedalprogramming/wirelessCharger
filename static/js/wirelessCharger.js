e4dfaabef9236155377fd7bbb107514c0a777115
yeonjudkwl <hyj9785@likelion.org>
2021-02-04 22:04:28 +0900

    wireless charger

---
+++ static/js/wirelessCharger.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {scrollIndicator()};
+
+function scrollIndicator() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.querySelector("#progressBar").style.width = scrolled + "%";
+}
---
+++ wirelessCharger.md
@@ -0,0 +1,5 @@
+# 무선충전기 랜딩페이지
+
+
+
+![iPhone_X_XS_11_Pro__3](..\static\img\iPhone_X_XS_11_Pro__3.png)
